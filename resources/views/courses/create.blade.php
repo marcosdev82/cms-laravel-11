82af1412703d41e3fc9492150ab02f0ae52fa093
mikek182 <marcosvinicius@geekwork.com.br>
2025-01-03 17:43:01 -0300

    html das views

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -1,3 +1,15 @@
-<div>
-    <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
-</div>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cria curso</title>
+</head>
+
+<body>
+    <h2>Criar curso</h2>
+</body>
+
+</html>
