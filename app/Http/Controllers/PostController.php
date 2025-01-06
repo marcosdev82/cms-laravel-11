9fff28ae5546d98689f9f1b95bce2fe23a55dff4
mikek182 <marcosvinicius@geekwork.com.br>
2025-01-06 14:01:14 -0300

    Correação post type

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -66,7 +66,7 @@ class PostController extends Controller
             'post_parent' => $request->post_parent ?: 0,
             'guid' => $request->guid ?: '',
             'menu_order' => $request->menu_order ?: 0,
-            'post_type' => $type,
+            'post_type' => $this->type,
             'post_mime_type' => $request->post_mime_type ?: '',
             'comment_count' => $request->comment_count ?: 0,
         ]);
